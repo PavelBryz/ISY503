f3da0d8976b540ff8a991a2d19c2058ce1379665
Thaila El Kadre <thaila.paganini@dnx.solutions>
2023-12-04 21:45:56 +1030

    improve the build_model and refactoring the code

--- model.py
+++ model.py
@@ -1,168 +1,137 @@
-import pandas as pd # data analysis toolkit - create, read, update, delete datasets
-import numpy as np #matrix math
-from sklearn.model_selection import train_test_split #to split out training and testing data 
-#keras is a high level wrapper on top of tensorflow (machine learning library)
-#The Sequential container is a linear stack of layers
-from keras.models import Sequential
-#popular optimization strategy that uses gradient descent 
-from keras.optimizers import Adam
-#to save our model periodically as checkpoints for loading later
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
 from keras.callbacks import ModelCheckpoint
-#what types of layers do we want our model to have?
-from keras.layers import Lambda, Conv2D, MaxPooling2D, Dropout, Dense, Flatten
-#helper class to define input shape and generate training images given image paths & steering angles
+from keras.layers import Conv2D, MaxPooling2D, Dense, Activation, InputLayer, BatchNormalization, GlobalAveragePooling2D
+from keras.models import Sequential
+from keras.optimizers import Adam
+from sklearn.model_selection import train_test_split
+
 from data_preprocessing import batch_generator
-#for command line arguments
-import argparse
-#for reading files
-import os
 
-IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNELS = 66, 200, 3
-INPUT_SHAPE = (IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNELS)
+# np.random.seed(0)
 
-#for debugging, allows for reproducible (deterministic) results 
-np.random.seed(0)
+INIT_LR = 1e-3
+BATCH_SIZE = 32
+EPOCHS = 120
 
 
-def load_data(args):
-    """
-    Load training data and split it into training and validation set
-    """
-    #reads CSV file into a single dataframe variable
-    #data_df = pd.read_csv(os.path.join(os.getcwd(), args.data_dir, r'C:\Users\pavel\OneDrive - Torrens Global Education Services\University\Trimester 3\Intelligent Systems\Assesment3\beta_simulator_windows\data\driving_log.csv'), names=['center', 'left', 'right', 'steering', 'throttle', 'reverse', 'speed'])
-    data_df = pd.read_csv(os.path.join(os.getcwd(), args.data_dir,
-                                       '/Users/rimma_vakhreeva/PycharmProjects/ISY503/beta_simulator_windows/data/updated_driving_log.csv'),
-                          names=['center', 'left', 'right', 'steering', 'throttle', 'reverse', 'speed'])
+def load_data():
+    data_dir = Path(__file__).parent / 'assets' / 'data'
+    print("test - ", data_dir)
 
-    #yay dataframes, we can select rows and columns by their names
-    #we'll store the camera images as our input data
-    X = data_df[['center', 'left', 'right']]
-    #and our steering commands as our output data
-    y = data_df['steering']
+    # Check if the file exists
+    if data_dir.exists():
+        print("File exists.")
+    else:
+        print("File doesn't exist or path is incorrect.")
 
-    #now we can split the data into a training (80), testing(20), and validation set
-    #thanks scikit learn
-    X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=args.test_size, random_state=0)
+    print(data_dir)
+    driving_data = pd.read_csv(data_dir / 'updated_driving_log.csv')
+    print(driving_data)
+    X = driving_data[['center', 'left', 'right']]
+    y = driving_data[['steering']]
+
+    X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=0.30, random_state=0)
 
     return X_train, X_valid, y_train, y_valid
 
 
-def build_model(args):
-    """
-    NVIDIA model used
-    Image normalization to avoid saturation and make gradients work better.
-    Convolution: 5x5, filter: 24, strides: 2x2, activation: ELU
-    Convolution: 5x5, filter: 36, strides: 2x2, activation: ELU
-    Convolution: 5x5, filter: 48, strides: 2x2, activation: ELU
-    Convolution: 3x3, filter: 64, strides: 1x1, activation: ELU
-    Convolution: 3x3, filter: 64, strides: 1x1, activation: ELU
-    Drop out (0.5)
-    Fully connected: neurons: 100, activation: ELU
-    Fully connected: neurons: 50, activation: ELU
-    Fully connected: neurons: 10, activation: ELU
-    Fully connected: neurons: 1 (output)
-
-    # the convolution layers are meant to handle feature engineering
-    the fully connected layer for predicting the steering angle.
-    dropout avoids overfitting
-    ELU(Exponential linear unit) function takes care of the Vanishing gradient problem. 
-    """
+def make_model():
     model = Sequential()
-    model.add(Lambda(lambda x: x/127.5-1.0, input_shape=INPUT_SHAPE))
-    model.add(Conv2D(24, 5, 5, activation='elu'))
-    # model.add(Conv2D(36, 5, 5, activation='elu'))
-    # model.add(Conv2D(48, 5, 5, activation='elu'))
-    # model.add(Conv2D(64, 3, 3, activation='elu'))
-    # model.add(Conv2D(64, 3, 3, activation='elu'))
-    model.add(Dropout(args.keep_prob))
-    model.add(Flatten())
-    model.add(Dense(100, activation='elu'))
-    model.add(Dense(50, activation='elu'))
-    model.add(Dense(10, activation='elu'))
-    model.add(Dense(1))
+    model.add(InputLayer(input_shape=(32, 32, 3)))
+
+    model.add(Conv2D(8, kernel_size=(3, 3), input_shape=(32, 32, 3), padding='same', use_bias=False))
+    model.add(BatchNormalization())
+    model.add(Activation('elu'))
+
+    model.add(Conv2D(8, kernel_size=(3, 3), input_shape=(32, 32, 3), padding='same', use_bias=False))
+    model.add(BatchNormalization())
+    model.add(Activation('elu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Conv2D(16, kernel_size=(3, 3), padding='same', use_bias=False))
+    model.add(BatchNormalization())
+    model.add(Activation('elu'))
+
+    model.add(Conv2D(16, kernel_size=(3, 3), padding='same', use_bias=False))
+    model.add(BatchNormalization())
+    model.add(Activation('elu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Conv2D(32, kernel_size=(3, 3), padding='same', use_bias=False))
+    model.add(BatchNormalization())
+    model.add(Activation('elu'))
+
+    model.add(Conv2D(32, kernel_size=(3, 3), padding='same', use_bias=False))
+    model.add(BatchNormalization())
+    model.add(Activation('elu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Conv2D(64, kernel_size=(3, 3), padding='same', use_bias=False))
+    model.add(BatchNormalization())
+    model.add(Activation('elu'))
+
+    model.add(Conv2D(64, kernel_size=(3, 3), padding='same', use_bias=False))
+    model.add(BatchNormalization())
+    model.add(Activation('elu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Conv2D(128, kernel_size=(3, 3), padding='same', use_bias=False))
+    model.add(BatchNormalization())
+    model.add(Activation('elu'))
+
+    model.add(Conv2D(128, kernel_size=(3, 3), padding='same', use_bias=False))
+    model.add(BatchNormalization())
+    model.add(Activation('elu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(GlobalAveragePooling2D())
+    model.add(Dense(128, use_bias=False))
+    model.add(BatchNormalization())
+    model.add(Activation('elu'))
     model.summary()
 
     return model
 
 
-def train_model(model, args, X_train, X_valid, y_train, y_valid):
-    """
-    Train the model
-    """
-    #Saves the model after every epoch.
-    #quantity to monitor, verbosity i.e logging mode (0 or 1), 
-    #if save_best_only is true the latest best model according to the quantity monitored will not be overwritten.
-    #mode: one of {auto, min, max}. If save_best_only=True, the decision to overwrite the current save file is
-    # made based on either the maximization or the minimization of the monitored quantity. For val_acc, 
-    #this should be max, for val_loss this should be min, etc. In auto mode, the direction is automatically
-    # inferred from the name of the monitored quantity.
+def train_model(model, X_train, X_valid, y_train, y_valid):
     checkpoint = ModelCheckpoint('model-{epoch:03d}.h5',
                                  monitor='val_loss',
                                  verbose=0,
-                                 save_best_only=args.save_best_only,
+                                 save_best_only=True,
                                  mode='auto')
 
-    #calculate the difference between expected steering angle and actual steering angle
-    #square the difference
-    #add up all those differences for as many data points as we have
-    #divide by the number of them
-    #that value is our mean squared error! this is what we want to minimize via
-    #gradient descent
-    model.compile(loss='mean_squared_error', optimizer=Adam(lr=args.learning_rate))
+    model.compile(loss='mean_squared_error', optimizer=Adam(lr=INIT_LR))
 
-    #Fits the model on data generated batch-by-batch by a Python generator.
+    # Prepare training and validation data using batch_generator
+    train_generator = batch_generator(X_train, y_train, BATCH_SIZE,
+                                      use_augmentations=True,
+                                      use_left=False,
+                                      use_right=False)
 
-    #The generator is run in parallel to the model, for efficiency. 
-    #For instance, this allows you to do real-time data augmentation on images on CPU in 
-    #parallel to training your model on GPU.
-    #so we reshape our data into their appropriate batches and train our model simulatenously
-    model.fit_generator(batch_generator(args.data_dir, X_train, y_train, args.batch_size, True),
-                        args.samples_per_epoch,
-                        args.nb_epoch,
-                        validation_data=batch_generator(args.data_dir, X_valid, y_valid, args.batch_size, False),
+    valid_generator = batch_generator(X_valid, y_valid, BATCH_SIZE,
+                                      use_augmentations=False,
+                                      use_left=False,
+                                      use_right=False)
+
+    model.fit_generator(train_generator,
+                        steps_per_epoch=len(X_train) // BATCH_SIZE,
+                        epochs=EPOCHS,
+                        validation_data=valid_generator,
+                        validation_steps=len(X_valid) // BATCH_SIZE,
                         callbacks=[checkpoint],
                         verbose=1)
 
-#for command line args
-def s2b(s):
-    """
-    Converts a string to boolean value
-    """
-    s = s.lower()
-    return s == 'true' or s == 'yes' or s == 'y' or s == '1'
-
 
 def main():
-    """
-    Load train/validation data set and train the model
-    """
-    parser = argparse.ArgumentParser(description='Behavioral Cloning Training Program')
-    parser.add_argument('-d', help='data directory',        dest='data_dir',          type=str,   default='data')
-    parser.add_argument('-t', help='test size fraction',    dest='test_size',         type=float, default=0.2)
-    parser.add_argument('-k', help='drop out probability',  dest='keep_prob',         type=float, default=0.5)
-    parser.add_argument('-n', help='number of epochs',      dest='nb_epoch',          type=int,   default=10)
-    parser.add_argument('-s', help='samples per epoch',     dest='samples_per_epoch', type=int,   default=20000)
-    parser.add_argument('-b', help='batch size',            dest='batch_size',        type=int,   default=40)
-    parser.add_argument('-o', help='save best models only', dest='save_best_only',    type=s2b,   default='true')
-    parser.add_argument('-l', help='learning rate',         dest='learning_rate',     type=float, default=1.0e-4)
-    args = parser.parse_args()
 
-    #print parameters
-    print('-' * 30)
-    print('Parameters')
-    print('-' * 30)
-    for key, value in vars(args).items():
-        print('{:<20} := {}'.format(key, value))
-    print('-' * 30)
 
-    #load data
-    data = load_data(args)
-    #build model
-    model = build_model(args)
-    #train model on data, it saves as model.h5 
-    train_model(model, args, *data)
+    data = load_data()
+    model = make_model()
+    train_model(model, *data)
 
 
 if __name__ == '__main__':
     main()
-
