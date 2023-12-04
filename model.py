8302872e2f8a3ebf62d12d1fb55ec2a2c76b0238
PSweeterB <aisiteb@gmail.com>
2023-12-04 21:19:55 +1030

    Example of working model file

--- model.py
+++ model.py
@@ -11,16 +11,13 @@ from keras.callbacks import ModelCheckpoint
 #what types of layers do we want our model to have?
 from keras.layers import Lambda, Conv2D, MaxPooling2D, Dropout, Dense, Flatten
 #helper class to define input shape and generate training images given image paths & steering angles
-from data_preprocessing import batch_generator
+from utils import INPUT_SHAPE, batch_generator
 #for command line arguments
 import argparse
 #for reading files
 import os
 
-IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNELS = 66, 200, 3
-INPUT_SHAPE = (IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNELS)
-
-#for debugging, allows for reproducible (deterministic) results 
+#for debugging, allows for reproducible (deterministic) results
 np.random.seed(0)
 
 
@@ -29,16 +26,13 @@ def load_data(args):
     Load training data and split it into training and validation set
     """
     #reads CSV file into a single dataframe variable
-    #data_df = pd.read_csv(os.path.join(os.getcwd(), args.data_dir, r'C:\Users\pavel\OneDrive - Torrens Global Education Services\University\Trimester 3\Intelligent Systems\Assesment3\beta_simulator_windows\data\driving_log.csv'), names=['center', 'left', 'right', 'steering', 'throttle', 'reverse', 'speed'])
-    data_df = pd.read_csv(os.path.join(os.getcwd(), args.data_dir,
-                                       '/Users/rimma_vakhreeva/PycharmProjects/ISY503/beta_simulator_windows/data/updated_driving_log.csv'),
-                          names=['center', 'left', 'right', 'steering', 'throttle', 'reverse', 'speed'])
+    data_df = pd.read_csv(os.path.join(os.getcwd(), args.data_dir, r'beta_simulator_windows\data\driving_log.csv'), names=['center', 'left', 'right', 'steering', 'throttle', 'reverse', 'speed'])
 
     #yay dataframes, we can select rows and columns by their names
     #we'll store the camera images as our input data
-    X = data_df[['center', 'left', 'right']]
+    X = data_df[['center', 'left', 'right']].values
     #and our steering commands as our output data
-    y = data_df['steering']
+    y = data_df['steering'].values
 
     #now we can split the data into a training (80), testing(20), and validation set
     #thanks scikit learn
@@ -65,21 +59,19 @@ def build_model(args):
     # the convolution layers are meant to handle feature engineering
     the fully connected layer for predicting the steering angle.
     dropout avoids overfitting
-    ELU(Exponential linear unit) function takes care of the Vanishing gradient problem. 
+    ELU(Exponential linear unit) function takes care of the Vanishing gradient problem.
     """
-    model = Sequential()
-    model.add(Lambda(lambda x: x/127.5-1.0, input_shape=INPUT_SHAPE))
-    model.add(Conv2D(24, 5, 5, activation='elu'))
-    # model.add(Conv2D(36, 5, 5, activation='elu'))
-    # model.add(Conv2D(48, 5, 5, activation='elu'))
-    # model.add(Conv2D(64, 3, 3, activation='elu'))
-    # model.add(Conv2D(64, 3, 3, activation='elu'))
-    model.add(Dropout(args.keep_prob))
-    model.add(Flatten())
-    model.add(Dense(100, activation='elu'))
-    model.add(Dense(50, activation='elu'))
-    model.add(Dense(10, activation='elu'))
-    model.add(Dense(1))
+    model = Sequential([
+        Lambda(lambda x: x / 127.5 - 1.0, input_shape=INPUT_SHAPE),
+        Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)),
+        MaxPooling2D(2, 2),
+        Conv2D(64, (3, 3), activation='relu'),
+        MaxPooling2D(2, 2),
+        Flatten(),
+        Dense(128, activation='relu'),
+        Dropout(0.5),
+        Dense(1)
+    ])
     model.summary()
 
     return model
@@ -90,16 +82,15 @@ def train_model(model, args, X_train, X_valid, y_train, y_valid):
     Train the model
     """
     #Saves the model after every epoch.
-    #quantity to monitor, verbosity i.e logging mode (0 or 1), 
+    #quantity to monitor, verbosity i.e logging mode (0 or 1),
     #if save_best_only is true the latest best model according to the quantity monitored will not be overwritten.
     #mode: one of {auto, min, max}. If save_best_only=True, the decision to overwrite the current save file is
-    # made based on either the maximization or the minimization of the monitored quantity. For val_acc, 
+    # made based on either the maximization or the minimization of the monitored quantity. For val_acc,
     #this should be max, for val_loss this should be min, etc. In auto mode, the direction is automatically
     # inferred from the name of the monitored quantity.
     checkpoint = ModelCheckpoint('model-{epoch:03d}.h5',
                                  monitor='val_loss',
                                  verbose=0,
-                                 save_best_only=args.save_best_only,
                                  mode='auto')
 
     #calculate the difference between expected steering angle and actual steering angle
@@ -112,14 +103,14 @@ def train_model(model, args, X_train, X_valid, y_train, y_valid):
 
     #Fits the model on data generated batch-by-batch by a Python generator.
 
-    #The generator is run in parallel to the model, for efficiency. 
-    #For instance, this allows you to do real-time data augmentation on images on CPU in 
+    #The generator is run in parallel to the model, for efficiency.
+    #For instance, this allows you to do real-time data augmentation on images on CPU in
     #parallel to training your model on GPU.
     #so we reshape our data into their appropriate batches and train our model simulatenously
     model.fit_generator(batch_generator(args.data_dir, X_train, y_train, args.batch_size, True),
                         args.samples_per_epoch,
                         args.nb_epoch,
-                        validation_data=batch_generator(args.data_dir, X_valid, y_valid, args.batch_size, False),
+                        # validation_data=batch_generator(args.data_dir, X_valid, y_valid, args.batch_size, False),
                         callbacks=[checkpoint],
                         verbose=1)
 
