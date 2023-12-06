09ad161854ebbdb4bab2b0fc2a1a24de631b996a
Thaila El Kadre <thaila.paganini@dnx.solutions>
2023-12-06 12:32:38 +1030

    refactoring file

--- model_new_rimma.py
+++ model_new_rimma.py
@@ -11,28 +11,30 @@ from keras.callbacks import ModelCheckpoint
 from keras.layers import Lambda, Conv2D, Dropout, Dense, Flatten
 from keras.models import Sequential
 from keras.optimizers import Adam
-from keras.src.layers import MaxPooling2D
 from sklearn.model_selection import train_test_split
 
-TEST_SIZE = 0.2
-DROPOUT_RATE = 0.5
-NB_EPOCH = 10
-SAMPLES_PER_EPOCH = 5000
+
+# constants
+TEST_SIZE = 0.3
 BATCH_SIZE = 32
-SAVE_BEST_ONLY = True
 LEARNING_RATE = 1E-3
-IMAGE_HEIGHT = 66
-IMAGE_WIDTH = 200
-IMAGE_CHANNELS = 3
-INPUT_SHAPE = (IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNELS)
 
 
 def get_driving_data(data_dir):
+    '''
+    Get the data from csv file
+    :param data_dir: data directory
+    :return: data
+    '''
     return pd.read_csv(data_dir / 'driving_log.csv', names=['center', 'left', 'right', 'steering', 'throttle',
                                                             'reverse', 'speed'])
 
 
 def get_data_folder():
+    '''
+    get the data folder
+    :return: data folder path
+    '''
     data_dir = Path(__file__).parent / 'assets' / 'data'
 
     # Check if the file exists
@@ -45,6 +47,11 @@ def get_data_folder():
 
 
 def get_checkpoint_filepath():
+    '''
+    create the file model.h5
+    verify the results folder exist if not create it
+    :return: return the model.h5 filepath
+    '''
     result_folder = 'results'
 
     if not os.path.exists(result_folder):
@@ -54,6 +61,13 @@ def get_checkpoint_filepath():
 
 
 def load_image(image_path: Path) -> np.ndarray:
+    """
+        Load an image from a given path and convert it from BGR to RGB color space.
+
+        :param image_path: The path to the image file.
+        :return: The loaded image in RGB format or None if the path is invalid.
+    """
+
     # Load image using OpenCV
     image = cv2.imread(str(image_path))
     assert image is not None, f"{image_path}"
@@ -69,13 +83,28 @@ def preprocess_img(
         image_width: int,
         image_height: int
 ) -> np.ndarray:
+    """
+        Preprocess the image by cropping and resizing it.
+
+        :param image: The input image.
+        :return: The preprocessed image.
+    """
+
     # Crop and resize the image
     image = image[60:-25, :, :]
     image = cv2.resize(image, (image_width, image_height), cv2.INTER_AREA)
+
     return image
 
 
 def apply_augmentations(image: np.ndarray) -> np.ndarray:
+    """
+        Apply a series of augmentations to the image to enhance the dataset variety.
+
+        :param image: The input image.
+        :return: The augmented image.
+    """
+
     # Seed for reproducibility of augmentations
     random.seed(42)
 
@@ -98,12 +127,28 @@ def apply_augmentations(image: np.ndarray) -> np.ndarray:
 
 
 def normalize_image(image: np.ndarray) -> np.ndarray:
+    """
+            Normalize the image by scaling pixel values to the range [0, 1].
+
+            :param image: The input image.
+            :return: Normalized image.
+    """
+
     # Assert that the image is not empty
     assert image is not None, "Image is None, cannot normalize."
     return image / 255.0
 
 
 def prepare_image(path: str, is_eval, image_width, image_height):
+    '''
+    Prepares an image for processing based on the given parameters.
+
+    :param path: The path to the image file.
+    :param is_eval: A boolean flag indicating whether the image is for evaluation (True) or training (False).
+    :param image_width: The desired width of the image.
+    :param image_height: The desired height of the image.
+    :return: The prepared image ready for further processing.
+    '''
     image = preprocess_img(load_image(Path(path)), image_width=image_width, image_height=image_height)
     if not is_eval:
         image = apply_augmentations(image)
@@ -121,6 +166,17 @@ def batch_generator(
         image_height: int = 66,
         image_width: int = 200
 ):
+    """
+        Generator function that yields batches of images and corresponding steering angles.
+
+        :param data: DataFrame containing image paths and steering angles.
+        :param batch_size: Number of image sets to include in each batch.
+        :param is_eval: ...
+        :param use_left: Boolean indicating whether to include left images.
+        :param use_right: Boolean indicating whether to include right images.
+        :yield: A batch of images and corresponding steering angles.
+    """
+
     for epoch in count(0, 1):
         if is_eval and epoch >= 1:
             break
@@ -157,17 +213,29 @@ def batch_generator(
 
 
 def load_data(data_dir) -> Dict[str, Any]:
+    '''
+    Load training data and split it into training and validation set
+    :param data_dir:  data directory
+    :return: the x and y train and valid
+    '''
     driving_data = get_driving_data(data_dir)
 
     X = driving_data[['center', 'left', 'right']]
     y = driving_data[['steering']]
 
-    X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=0.30, random_state=0)
+    X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=TEST_SIZE, random_state=0)
 
     return X_train, X_valid, y_train, y_valid
 
 
-def build_model(activation_type, x_train):
+def build_model(activation_type):
+    '''
+    Creates a convolutional neural network model based on NVIDIA's architecture.
+
+    :param activation_type: The type of activation function to be used in the model's layers.
+    :return: A convolutional neural network model based on the specified activation function.
+    '''
+
     model = Sequential([
         Lambda(lambda x: x / 127.5 - 1.0, input_shape=(66, 200, 3)),
         Conv2D(24, (5, 5), strides=(2, 2), activation=activation_type, input_shape=(28, 28, 1)),
@@ -182,33 +250,52 @@ def build_model(activation_type, x_train):
         Dense(10, activation=activation_type),
         Dense(1)
     ])
+
     model.summary()
 
     return model
 
 
 def compile_model(model):
+    '''
+    Compiles the given neural network model using specified loss function and optimizer.
+
+    :param model: The neural network model to be compiled.
+    '''
     model.compile(loss='mean_squared_error', optimizer=Adam(learning_rate=LEARNING_RATE))
 
+def create_data_generator(X_data, y_data, is_eval=False):
+    '''
+    Creates a data generator using batch_generator for training or validation.
 
-def create_train_generator(X_train, y_train):
-    return batch_generator(X_train, y_train, batch_size=BATCH_SIZE,
-                           is_eval=False, use_left=False, use_right=False)
-
-
-def create_valid_generator(X_valid, y_valid):
-    return batch_generator(X_valid, y_valid, batch_size=BATCH_SIZE,
-                           is_eval=True, use_left=False, use_right=False)
+    :param X_data: The dataset (input features).
+    :param y_data: The dataset (target labels).
+    :param is_eval: A flag indicating whether the generator is for evaluation (default: False).
+    :return: A generator for data batches.
+    '''
+    return batch_generator(X_data, y_data, batch_size=BATCH_SIZE,
+                           is_eval=is_eval, use_left=False, use_right=False)
 
 
 def train_model(model, X_train, X_valid, y_train, y_valid):
+    '''
+       Trains the given model using the provided training and validation datasets.
+
+       :param model: The neural network model to be trained.
+       :param X_train: The training dataset (input features).
+       :param X_valid: The validation dataset (input features).
+       :param y_train: The training dataset (target labels).
+       :param y_valid: The validation dataset (target labels).
+       :return: None
+    '''
+
     file_path = get_checkpoint_filepath()
     checkpoint = ModelCheckpoint(file_path, monitor='val_loss', verbose=0, mode='auto')
 
     compile_model(model)
 
-    train_generator = create_train_generator(X_train, y_train)
-    valid_generator = create_valid_generator(X_valid, y_valid)
+    train_generator = create_data_generator(X_train, y_train)
+    valid_generator = create_data_generator(X_valid, y_valid, is_eval=True)
 
     model.fit_generator(train_generator,
                         validation_data=valid_generator,
@@ -220,11 +307,22 @@ def train_model(model, X_train, X_valid, y_train, y_valid):
 
 
 def main():
+    '''
+        Main function to execute the training process of a neural network model.
+
+        This function performs the following steps:
+        1. Retrieves the data directory using get_data_folder().
+        2. Loads the training and validation datasets using load_data().
+        3. Builds a neural network model using build_model() with 'elu' as the activation function.
+        4. Trains the model using train_model() with the loaded datasets.
+
+    '''
+
     data_dir = get_data_folder()
 
     X_train, X_valid, y_train, y_valid = load_data(data_dir)
 
-    model = build_model('elu', X_train)
+    model = build_model('elu')
 
     train_model(
         model,
