ca22dd8d4151df0d3bac2ce4bbb2280f53431c50
PSweeterB <aisiteb@gmail.com>
2023-12-06 14:11:50 +1030

    Final version of drive.py (v3)

--- drive.py
+++ drive.py
@@ -11,12 +11,19 @@ from flask import Flask
 from keras.models import load_model
 
 # Path to the trained model file.
-MODEL_PATH = "model.h5"
+# MODEL_PATH = r".\results\model-106.h5"
+MODEL_PATH = r".\model.h5"
 
 
 class TelemetryServer:
+    """
+    Class responsible for connecting to app server and send signals
+    """
     def __init__(self, model):
-        # Initializing server and wrapping Flask with socketio.
+        """
+        Initializing server and wrapping Flask with socketio.
+        :param model: Model that will be passed used to predict steering angle
+        """
         self.sio = socketio.Server()
         self.app = socketio.Middleware(self.sio, Flask(__name__))
         # Registering event handlers for socket connections and telemetry data.
@@ -29,13 +36,23 @@ class TelemetryServer:
         eventlet.wsgi.server(eventlet.listen(('', 4567)), self.app)
 
     def connect(self, sid, environ):
-        # Handling new connections.
+        """
+        Handling new connections.
+        :param sid:
+        :param environ:
+        :return:
+        """
         print("connect ", sid)
         # Sending initial control commands upon connection.
         self._send_control(0, 1)
 
     def telemetry(self, sid, data):
-        # Handling telemetry data received from client.
+        """
+        Handling telemetry data received from client.
+        :param sid:
+        :param data:
+        :return:
+        """
         if data:
             # Processing the data to get control values.
             self.driver.get_properties_from_data(data)
@@ -46,7 +63,12 @@ class TelemetryServer:
             self.sio.emit('manual', data={}, skip_sid=True)
 
     def _send_control(self, steering_angle, throttle):
-        # Sending control commands to the client.
+        """
+        Sending control commands to the client.
+        :param steering_angle:
+        :param throttle:
+        :return:
+        """
         print(f"{steering_angle=} | {throttle=}")
         self.sio.emit(
             "steer",
@@ -58,8 +80,16 @@ class TelemetryServer:
 
 
 class Driver:
-    def __init__(self, model, max_speed: int = 25, min_speed: int = 10):
-        # Initializing driver with speed limits and model.
+    """
+    Used to define parameters for driving simulated car
+    """
+    def __init__(self, model, max_speed: int = 20, min_speed: int = 10):
+        """
+        Initializing driver with speed limits and model.
+        :param model: Model that will be passed used to predict steering angle
+        :param max_speed: Limit on max speed
+        :param min_speed: Limit on min speed
+        """
         self.imgString = None
         self.speed = None
         self.max_speed = max_speed
@@ -68,18 +98,28 @@ class Driver:
         self.model = model
 
     def get_properties_from_data(self, data):
-        # Extracting speed and image data from telemetry data.
+        """
+        Extracting speed and image data from telemetry data.
+        :param data: Data to parse
+        :return:
+        """
         self.speed = float(data["speed"])
         self.imgString = data["image"]
 
     @property
     def throttle(self):
-        # Calculating throttle based on current speed and max speed.
+        """
+        Calculating throttle based on current speed and max speed.
+        :return:
+        """
         return 1.2 - (self.speed / self.max_speed)
 
     @property
     def steering_angle(self):
-        # Predicting the steering angle from the image using the model.
+        """
+        Predicting the steering angle from the image using the model.
+        :return:
+        """
         if self.model:
             image = Image.open(BytesIO(base64.b64decode(self.imgString)))
             image_array = np.asarray(image)
